2e273f405a6bae621db84c12d51694be6577d9af
Jason Ranjit <jasonranjit@gmail.com>
2026-07-10 00:06:56 -0500

    procedures

---
+++ 2_Verilog_Language/4_Procedures/Always_case2.v
@@ -0,0 +1,16 @@
+module top_module (
+    input [3:0] in,
+    output reg [1:0] pos  );
+    
+    always@(*) begin
+        casez(in)
+            4'd0: pos = 0;
+            4'b???1: pos = 2'b00;
+            4'b??10: pos = 2'b01;
+            4'b?100: pos = 2'b10;
+            4'b1000: pos = 2'b11;
+        endcase
+    end
+
+endmodule
+
